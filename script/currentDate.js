f84df099a09fb3696a5927140f7f8c9b91b4ff61
esmotara-codec <esmotara1725@gmail.com>
2025-03-14 13:13:09 +0600

    Current date updated

---
+++ script/currentDate.js
@@ -0,0 +1,36 @@
+function displayCurrentDate(){
+    const today = new Date();
+    console.log("Full date from JavaScript:", today.toLocaleString());
+
+    //Current Day
+    const days =["Sat", "Sun" , "Mon" ,"Tue", "Wed", "Thu" ,"Fri"];
+    const daysOfWeek = days[today.getDay()];
+
+     //Current Day
+      const date =today.getDate();
+
+        //Current month
+        const months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", 
+                   "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const month = months[today.getMonth()];
+
+
+        const year =today.getFullYear();
+
+        // const dateString = `${daysOfWeek}, ${month}, ${date}, ${year}`;
+
+
+        document.getElementById("current-day").innerText = daysOfWeek;
+        document.getElementById("current-month").innerText = month;
+        document.getElementById("current-date").innerText = date;
+        document.getElementById("current-year").innerText = year;
+        
+        
+
+  
+
+
+}
+
+window.addEventListener("load", displayCurrentDate);
+setInterval(displayCurrentDate, 60000); // Update every minute
